be7178534ce6f137d17e1851b7588a03ff927103
shishkin1966 <shishkin1966@gmail.com>
2019-08-31 20:16:31 +0300

    Доработка

--- app/src/main/java/shishkin/sl/kodeinpsb/sl/ApplicationSpecialist.kt
+++
@@ -1,9 +0,0 @@
-package shishkin.sl.kodeinpsb.sl
-
-import android.app.Application
-
-class ApplicationSpecialist : Application() {
-    companion object{
-        val instance = ApplicationSpecialist()
-    }
-}
